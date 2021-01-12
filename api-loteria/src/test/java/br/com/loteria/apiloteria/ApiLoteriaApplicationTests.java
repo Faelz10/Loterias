2fe21d7928b57331af3c963e52a9f68e7b6c537b
Rafael <rafaelazevedor@hotmail.comgit config --global user.email rafaelazevedor@hotmail.comgit config --global user.email rafaelazevedor@hotmail.com>
2021-01-12 20:55:07 -0300

    first commit

---
+++ api-loteria/src/test/java/br/com/loteria/apiloteria/ApiLoteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loteria.apiloteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLoteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
